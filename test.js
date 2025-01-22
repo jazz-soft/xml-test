2f1ef330ab42451f5f206b50bf4261d54c65223a
Sema <jazz-dev@jazz-soft.net>
2025-01-21 21:05:42 -0500

    DOMParser/XMLSerializer test

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
+const fs = require('fs');
 const PW = require('playwright');
 
 var n, s;
 const browsers = ['chromium', 'firefox', 'webkit', 'chrome', 'msedge'];
 var bro = {};
+var xml_file, xml_data;
 
 for (n = 2; n  < process.argv.length; n++) {
   s = process.argv[n];
   if (browsers.includes(s)) bro[s] = true;
+  else if (s.endsWith('.xml') && !xml_file) xml_file = s;
 }
+if (xml_file) xml_data = fs.readFileSync(xml_file, 'utf8');
+
 if (!Object.keys(bro).length) for (s of browsers) bro[s] = true;
 for (s of Object.keys(bro)) run(s);
 
@@ -16,13 +21,15 @@ async function run(br) {
   var done;
   var finish = new Promise((resolve) => { done = resolve; });
   const browser = await launch(br, true);
-  console.log(br, browser.version());
+  //console.log(br, browser.version());
 
   const page = await browser.newPage();
-  await page.addInitScript({ content: `(${inject.toString()})()` });
+  const data = {};
+  if (xml_data) data.xml = xml_data;
+  await page.addInitScript({ content: `(${inject.toString()})(${JSON.stringify(data)})` });
   page.on('console', function(msg) {
-    console.log(br, '>>', msg);
     if (msg.text() == 'Done!') done();
+    else console.log(br, '>>', msg);
   });
   await page.goto('about:blank');
   await finish;
@@ -49,6 +56,18 @@ async function launch(br, h) {
   }
 }
 
-function inject() {
+function inject(data) {
+  try {
+    if (data.xml) {
+      var parser = new DOMParser();
+      var serializer = new XMLSerializer();
+      var xml = parser.parseFromString(data.xml, "text/xml").documentElement;
+      var str = serializer.serializeToString(xml);
+      console.log('xml:', xml, str);
+    }
+  }
+  catch (e) {
+    console.log(e);
+  }
   console.log('Done!');
 }
