f437ad59b8551d27a01557511468282ddfb3eb8a
Sema <jazz-dev@jazz-soft.net>
2025-01-24 21:28:41 -0500

    Pass data to the test

--- test.js
+++ test.js
@@ -5,8 +5,7 @@ const PW = require('playwright');
 
 const browsers = ['chromium', 'firefox', 'webkit', 'chrome', 'msedge'];
 var n, s, x;
-var tst = {}, bro = {};
-var xml_file, xpath_file, xslt_file;
+var tst = {}, bro = {}, xml = {}, xpath = {}, xslt = {};
 
 const list = JSON.parse(fs.readFileSync(path.join(__dirname, 'tests.json'), 'utf8'));
 const groups = {};
@@ -15,30 +14,33 @@ for (x of list.groups) groups[x.name] = {};
 for (x of list.tests) {
   tests[x.name] = x;
   groups[x.name.split('/')[0]][x.name] = x;
+  if (x.xml) x.xml = path.join(__dirname, 'data', x.xml);
+  if (x.xpath) x.xpath = path.join(__dirname, 'data', x.xpath);
+  if (x.xslt) x.xslt = path.join(__dirname, 'data', x.xslt);
 }
 for (n = 2; n  < process.argv.length; n++) {
   s = process.argv[n];
   if (browsers.includes(s)) bro[s] = true;
-  else if (s.endsWith('.xml')) {
-    if (!xml_file) xml_file = s;
-    else quit('Only a single .xml file is allowed');
-  }
-  else if (s.endsWith('.xpath')) {
-    if (!xpath_file) xpath_file = s;
-    else quit('Only a single .xpath file is allowed');
-  }
-  else if (s.endsWith('.xslt') || s.endsWith('.xsl')) {
-    if (!xslt_file) xslt_file = s;
-    else quit('Only a single .xslt file is allowed');
-  }
+  else if (s.endsWith('.xml')) xml[s] = true;
+  else if (s.endsWith('.xpath')) xpath[s] = true;
+  else if (s.endsWith('.xslt') || s.endsWith('.xsl')) xslt[s] = true;
   else if (tests[s]) tst[s] = tests[s];
   else if (groups[s]) for (x of Object.keys(groups[s])) tst[x] = tests[x];
   else quit('Unknown command line option: ' + s);
 }
 if (!Object.keys(bro).length) for (s of browsers) bro[s] = true;
+if (Object.keys(xml).length) for (n of Object.keys(xml)) {
+  x = { name: 'from file: ' + n, xml: n };
+  tst[x.name] = x;
+console.log('new test: ', x);
+}
 if (!Object.keys(tst).length) tst = tests;
 
-for (x of Object.keys(tst)) {
+for (n of Object.keys(tst)) {
+  x = tst[n];
+  if (x.xml) x.xml = fs.readFileSync(x.xml, 'utf8');
+  if (x.xpath) x.xpath = fs.readFileSync(x.xpath, 'utf8');
+  if (x.xslt) x.xslt = fs.readFileSync(x.xslt, 'utf8');
   for (s of Object.keys(bro)) run(x, s);
 }
 
@@ -48,17 +50,12 @@ function quit(s) {
 }
 
 async function run(tst, br) {
-console.log(tst, br);
-return;
   var done;
   var finish = new Promise((resolve) => { done = resolve; });
   const browser = await launch(br, true);
-  //console.log(br, browser.version());
-
   const page = await browser.newPage();
   const data = {};
-  if (xml_data) data.xml = xml_data;
-  await page.addInitScript({ content: `(${inject.toString()})(${JSON.stringify(data)})` });
+  await page.addInitScript({ content: `(${inject.toString()})(${JSON.stringify(tst)})` });
   page.on('console', function(msg) {
     if (msg.text() == 'Done!') done();
     else console.log(br, '>>', msg);
