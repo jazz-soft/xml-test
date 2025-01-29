31244a1573cd29be98ccb4008b10f717d0d3c4e8
Sema <jazz-dev@jazz-soft.net>
2025-01-28 23:37:42 -0500

    Automatic README.md update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # xml-test
 coming soon...
+<table>
+<tr><th>❌ chromium</th><th>✅ chromium</th><th>❌ firefox</th><th>✅ firefox</th><th>❌ webkit</th><th>✅ webkit</th></tr>
+<tr><th colspan="6">DOMParser / XMLSerializer: Unicode characters in the tag name (4E00-9FFF)</th></tr>
+<tr><td></td><td>✅ 131.0</td><td></td><td>✅ 132.0</td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">DOMParser / XMLSerializer: Unicode characters in the tag name (1780-17FF)</th></tr>
+<tr><td></td><td>✅ 131.0</td><td>❌ 134.0</td><td></td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">DOMParser / XMLSerializer: Unicode characters in the attribute name (4E00-9FFF)</th></tr>
+<tr><td></td><td>✅ 131.0</td><td></td><td>✅ 132.0</td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">DOMParser / XMLSerializer: Unicode characters in the attribute name (1780-17FF)</th></tr>
+<tr><td></td><td>✅ 131.0</td><td>❌ 134.0</td><td></td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">DOMParser / XMLSerializer: Unicode characters in the text node (4E00-9FFF)</th></tr>
+<tr><td></td><td>✅ 131.0</td><td></td><td>✅ 132.0</td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">DOMParser / XMLSerializer: Unicode characters in the text node (1780-17FF)</th></tr>
+<tr><td></td><td>✅ 131.0</td><td></td><td>✅ 132.0</td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">DOMParser / XMLSerializer: Unicode characters in the attribute value (4E00-9FFF)</th></tr>
+<tr><td></td><td>✅ 131.0</td><td></td><td>✅ 132.0</td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">DOMParser / XMLSerializer: Unicode characters in the attribute value (1780-17FF)</th></tr>
+<tr><td></td><td>✅ 131.0</td><td></td><td>✅ 132.0</td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">Evaluate XPath expression: Simple math expression</th></tr>
+<tr><td></td><td>✅ 131.0</td><td></td><td>✅ 132.0</td><td></td><td>✅ 18.2</td></tr>
+<tr><th colspan="6">Evaluate XPath expression: Simple boolean expression</th></tr>
+<tr><td></td><td>✅ 131.0</td><td></td><td>✅ 132.0</td><td></td><td>✅ 18.2</td></tr>
+</table>
+to be continued...
--- update.js
+++ update.js
@@ -5,6 +5,7 @@ const path = require('path');
 const list_file = path.join(__dirname, 'tests.json');
 const hist_file = path.join(__dirname, 'data', 'history.json');
 const data_file = path.join(__dirname, 'test-result.json');
+const rdme_file = path.join(__dirname, 'README.md');
 const list = JSON.parse(fs.readFileSync(list_file, 'utf8'));
 const hist = fs.existsSync(hist_file) ? JSON.parse(fs.readFileSync(hist_file, 'utf8')) : {};
 const data = JSON.parse(fs.readFileSync(data_file, 'utf8'));
@@ -12,7 +13,11 @@ const report = { chromium: true, firefox: true, webkit: true };
 const new_hist = {};
 const order = [];
 const index = {};
-var k, x;
+const groups = {};
+const bros = ['chromium', 'firefox', 'webkit'];
+const rdme = [];
+const table = [];
+var k, x, s, w;
 for (x of list.tests) {
   order.push(x.name);
   index[x.name] = x;
@@ -22,7 +27,45 @@ for (x of data) if (index[x.test] && report[x.browser]) record(x);
 for (k of order) new_hist[k] = hist[k];
 fs.writeFileSync(hist_file, JSON.stringify(new_hist, null, 2), 'utf8');
 
+for (x of list.groups) groups[x.name] = x.descr;
+table.push('<table>');
+s = '';
+for (k of bros) s += `<th>❌ ${k}</th><th>✅ ${k}</th>`;
+table.push(`<tr>${s}</tr>`);
+for (x of list.tests) {
+  table.push(`<tr><th colspan="${bros.length * 2}">${groups[x.name.split('/')[0]]}: ${x.descr}</th></tr>`);
+  s = '';
+  for (k of bros) {
+    w = fail_pass(x.name, k);
+    s += `<td>${w[0] ? '❌ ' + w[0].split('.').slice(0, 2).join('.') : ''}</td><td>${w[1] ? '✅ ' + w[1].split('.').slice(0, 2).join('.') : ''}</td>`;
+  }
+  table.push(`<tr>${s}</tr>`);
+}
+table.push('</table>');
+x = true;
+for (s of fs.readFileSync(rdme_file, 'utf8').split(/\r?\n/)) {
+  if (s == '<table>') {
+    x = false;
+    rdme.push(table.join('\n'));
+  }
+  if (x) rdme.push(s);
+  if (s == '</table>') x = true;
+}
+
+fs.writeFileSync(rdme_file, rdme.join('\n'), 'utf8');
+//console.log(rdme.join('\n'));
+
 function record(x) {
   if (!hist[x.test]) hist[x.test] = { chromium: {}, firefox: {}, webkit: {} };
   hist[x.test][x.browser][x.ver] = x.pass;
+}
+
+function fail_pass(x, b) {
+  var f0, f1, p0, p1, n;
+  if (!hist[x] || !hist[x][b]) return [];
+  for (n of Object.keys(hist[x][b]).sort()) {
+    if (hist[x][b][n]) { f0 = undefined; p1 = n; if (!p0) p0 = n; }
+    else { p0 = undefined; f1 = n; if (!f0) f0 = n; }
+  }
+  return !f1 || !p1 || p1 > f1 ? [f1, p0] : [f0, p1];
 }
